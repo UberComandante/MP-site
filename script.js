7a4598db579353de29a9784e91fce2d5c1c58c84
UberComandante <ILoveWarface555@gmail.com>
2025-11-08 16:00:37 +0300

    Test consts

--- script.js
+++ script.js
@@ -16,3 +16,14 @@ function changeBackground() {
     bodyColor.classList.toggle("purple");
 }
 color.addEventListener("click", changeBackground);
+
+
+
+const buttonT = document.getElementById('buttonT');
+const area = document.getElementById('testArea');
+
+function test() {
+    area.innerHTML = "Hello friend!";
+}
+
+buttonT.addEventListener("click", test);
